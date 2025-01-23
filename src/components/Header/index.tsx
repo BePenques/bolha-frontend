9cd935de5bfec3647de79ce7f4a75cb1b72d77e0
Betiza Barreira <be.penques@hotmail.com>
2025-01-23 15:48:28 -0300

    feat: responsividade do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,6 @@
 import CategorySelect from "../CategorySelect";
 import SearchInput from "../SearchInput";
 
-
 interface HeaderProps {
     selectedCategory: number | 0;
     onCategoryChange: (categoryId: number) => void;
@@ -14,15 +13,6 @@ export default function Header({selectedCategory,onCategoryChange, onFilterChang
                 <div className="logo">           
                     <div className="imagem">LOGO</div>
                 </div>
-
-                <div className="filters">
-                    <SearchInput onChange={onFilterChange}/>
-                    <CategorySelect
-                        selectedCategory={selectedCategory}
-                        onChange={onCategoryChange}
-                    />
-                </div>
-
                 <div className="user-info">
                     <img className="user-profile" src="https://github.com/BePenques.png" alt="Avatar do usuário"/>
                     <div>
@@ -31,6 +21,15 @@ export default function Header({selectedCategory,onCategoryChange, onFilterChang
                             <span>Betiza Penques</span>
                         </div>
                     </div>                  
+                </div>
+            </div>
+            <div className="container-filters"> 
+                <div className="filters">
+                    <SearchInput onChange={onFilterChange}/>
+                    <CategorySelect
+                        selectedCategory={selectedCategory}
+                        onChange={onCategoryChange}
+                    />
                 </div>
             </div>
         </header>
